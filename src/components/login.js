741cd12c20505a10902a5f7b38f6d5fef962f058
Dharmika <dharmikathota@gmail.com>
2022-08-06 22:47:24 +0530

    login-register

--- src/components/login.js
+++ src/components/login.js
@@ -2,8 +2,8 @@ import "../styles/login.css";
 
 function Login(){
     return(
-        <div class="main">
-        <form class="box" action="" method="post">
+        <div className="main">
+        <form className="box" action="" method="post">
             <h1>Login</h1>
             <input type="text" name="" placeholder="Username" required/>
             <input type="password" name="" placeholder="Password" required/>
