72eb4aa0b938adb1d92de9b3cac160645db61b73
vennu-venu <vennuvenu2392@gmail.com>
2022-08-06 22:03:23 +0530

    bug-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Start from "./components/start";
+import { useAuth } from "./contexts/auth-context"
 
 function App() {
+
+  const { signup } = useAuth();
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    try {
+      // await signup((Math.random() + 1).toString(36).substring(7), (Math.random() + 1).toString(36).substring(7));
+      await signup("Ram", "1234");
+    } catch {
+      console.log("Error occurred");
+    }
+  }
+
   return (
     <div>
-      <Start/>
+      <button onClick={handleSignUp} className="btn">Add</button>
+      <Start />
     </div>
   );
 }
