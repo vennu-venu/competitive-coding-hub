b1b476b11888a16846710a9a67d9465d23b80d3e
vennu-venu <vennuvenu2392@gmail.com>
2022-08-07 23:28:05 +0530

    after-merge-basic-be

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import Register from "./components/register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  
-  const handleRegister = async() => {
-    try {
-      const user = await createUserWithEmailAndPassword(auth, "user@gmail.com", "pwd@1234");
-      console.log(user);
-    }
-    catch(error) {
-      console.log("Error in Sign Up: ", error.message);
-    }
-  }
-
   return (
     <div>
       <Routes>
